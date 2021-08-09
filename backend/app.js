107dc051f993c52fbd6e3979a81b8852b4bc3990
Tashik <k180142@nu.edu.pk>
2021-08-09 15:01:48 +0500

    updated

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = `${process.env.PORT}`;
 const homeRoute = require('./routes/home')
 const aboutRoute = require('./routes/about')
-const contactRoute = require('./routes/contact')
+const contactRoute = require('./routes/contact.js')
 
 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use('/',homeRoute); 
 app.use('/about',aboutRoute);
 app.use('/contact',contactRoute);
-app.listen(port, () => console.log('Listening on port 4000'));
+app.listen(port, () => console.log('Listening on port 8080'));
 
 
  
--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -1,20 +1,59 @@
+require('dotenv').config();
+const mysql = require('mysql2');
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send({data: "response from contact route"})
-});
 
-router.put("/", (req, res) => {
-    // some code
-});
+const database_connection = mysql.createPool( 
+    {
+        host: `${process.env.HOST}`,
+        user: `${process.env.USER}`,
+        password: `${process.env.PASSWORD}`,
+        database: `${process.env.DATABASE}`,
+        multipleStatements: true,
+        waitForConnections: true,
+        connectionLimit: 100,
+        queueLimit: 0,
+    }
+);
+
+
+const Add_Contact = async (information_data) => {
+    if (!information_data.Name || !information_data.Email || !information_data.Contact || !information_data.Subject || !information_data.Message) {
+        throw new Error('Please fill the required data and try again.');
+    }
+    const query = `INSERT INTO portfolio.Contact_Details (Name, Email, Contact, Subject, Message ) 
+          VALUES ('${information_data.Name}', '${information_data.Email}', 
+          '${information_data.Contact}', '${information_data.Subject}', '${information_data.Message}' );`;
+
+    try {
+        database_connection.query(query, (err, result) => {
+            console.log(err);
+        });
+    } 
+    catch (error) {
+        throw new Error(error.message);
+    }
+
+}
+
 
 router.post("/", (req, res) => {
-    // some code
-});
-
-router.delete("/", (req, res) => {
-    // some code
+    try {
+        const contact_information = 
+        {
+          Name: req.body.Name,
+          Email: req.body.Email,
+          Contact: req.body.Contact,
+          Subject: req.body.Subject,
+          Message: req.body.Message
+        };
+        Add_Contact(contact_information);
+        return res.status(200).json({ msg: 'Message sent successfully!' });
+    } 
+    catch (error) {
+        return res.status(500).json({ msg: `Unable to send your message, due to error "${error.message}"` });
+    }
 });
 
 module.exports = router;
